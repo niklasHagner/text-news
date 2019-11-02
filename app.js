e9386cf333a8cf385d928ca0a172167a5bc680c5
niklasHagner <niklas.hagner@outlook.com>
2019-11-02 10:40:55 +0100

    Rewrite logger

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const logger = require("./lib/logger.js");
 const packageInfo = require("./package.json");
 const setupApp = require("./lib/init/setupApp.js");
 const app = setupApp();
@@ -11,7 +10,7 @@ module.exports = app; // Expose app to tests
 if (require.main === module) {
   const port = Number(process.env.PORT) || 3000;
   const server = app.listen(port, () => {
-    logger.info("%s listening on port %d", packageInfo.name, server.address().port);
+    console.log("%s listening on port %d", packageInfo.name, server.address().port); // eslint-disable-line no-console
   });
 
   ["SIGTERM", "SIGINT", "SIGHUP"].forEach((evt) => process.once(evt, () => server.close(process.exit)));
